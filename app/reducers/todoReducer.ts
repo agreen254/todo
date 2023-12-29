25619f77fabbf5241d0516f91a1e5a3061915122
Aaron Green <aarongreen1997@gmail.com>
2023-12-28 22:58:45 -0600

    migrate Actions to types file

--- app/reducers/todoReducer.ts
+++ app/reducers/todoReducer.ts
@@ -1,53 +1,5 @@
+import { Actions } from "../types";
 import { Todo } from "../types";
-
-type AddTodo = {
-  actionName: "ADD_TODO";
-  toAdd: Todo;
-};
-
-type DeleteTodo = {
-  actionName: "DELETE_TODO";
-  toDelete: Todo;
-};
-
-type DeleteAllTodos = {
-  actionName: "DELETE_ALL_TODOS";
-};
-
-type EditTodo = {
-  actionName: "UNPIN_TODO";
-  toUnpin: Todo;
-};
-
-type CompleteTodo = {
-  actionName: "EDIT_TODO";
-  editedTodo: Todo;
-};
-
-type PinTodo = {
-  actionName: "PIN_TODO";
-  toPin: Todo;
-};
-
-type UnpinTodo = {
-  actionName: "COMPLETE_TODO";
-  toComplete: Todo;
-};
-
-type RevertTodo = {
-  actionName: "REVERT_TODO";
-  toRevert: Todo;
-};
-
-export type Actions =
-  | AddTodo
-  | DeleteTodo
-  | DeleteAllTodos
-  | EditTodo
-  | CompleteTodo
-  | RevertTodo
-  | PinTodo
-  | UnpinTodo;
 
 export function todoReducer(
   todos: Todo[],
